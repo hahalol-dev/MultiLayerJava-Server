b7a11e5ec62e26138843857b9ee5b496bf820bd2
hahalol-dev <mona.pers1996@gmail.com>
2024-10-14 14:42:20 +0300

    Create MultiFileServerApplication.java

---
+++ MultiFileServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.multifileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiFileServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiFileServerApplication.class, args);
+    }
+}
